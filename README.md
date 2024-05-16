def8eb8a45c2bcba9efb1e7afad23a9c97e1e150
Shawn <yxiaoa@qq.com>
2024-05-16 09:59:37 +0800

    Add ruby

--- README.md
+++ README.md
@@ -12,3 +12,32 @@ Vscode
 - Java
 - Ruby
 - Rust
+
+## Environment Setup
+
+### Ruby
+
+#### Run
+1. Install ruby, and use ruby -v to check
+2. Install vscode extension Ruby LSP
+3. Install dependency
+   1. gem install bundle
+   2. gem install bundler
+   3. gem install debase-ruby_core_source
+   4. gem install debase
+   5. gem install ruby-debug-ide
+4. Run in terminal
+   - ruby main.rb
+
+#### Debug
+
+TBD
+
+#### Unit test
+
+TBD
+
+#### Format & Lint
+
+TBD
+
---
+++ leetcodeRuby/main.rb
@@ -0,0 +1,49 @@
+require 'set'
+
+# @param {String} s
+# @return {String}
+def make_smallest_palindrome(s)
+	chars = s.split("")
+	i = 0
+	j = s.length - 1
+	while i < j
+	  if chars[i].ord < chars[j].ord
+	    chars[j] = chars[i]
+	  else
+	    chars[i] = chars[j]
+	  end
+	  i += 1
+	  j -= 1
+	end
+	chars.join
+end
+
+input0 = ["egcfe", "abcd", "seven"]
+input2 = [10, 1, 9]
+input3 = [1,2, 0, 3, 0,2]
+expect = ["efcfe", "abba", "neven"]
+
+fail_cnt = 0
+pass_cnt = 0
+
+puts "\n"
+for i in 0...input0.length
+        output = make_smallest_palindrome(input0[i])
+        if expect[i] != output
+                fail_cnt += 1
+                puts "Input #{i} expect #{expect[i]}, actual #{output}"
+        else
+                pass_cnt += 1
+        end
+end
+
+puts "==== Test Summary ===="
+if fail_cnt == 0
+        puts "TEST PASS!"
+else
+        puts "TEST FAIL!"
+end
+puts "test #{fail_cnt+pass_cnt}"
+puts "pass #{pass_cnt}"
+puts "fail #{fail_cnt}"
+puts "======================"
