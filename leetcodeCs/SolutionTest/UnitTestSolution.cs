e829b631b1d2276ddc75057da415c4ac554d751b
Shawn <yxiaoa@qq.com>
2024-05-29 09:35:38 +0800

    Rename unittest class

--- leetcodeCs/SolutionTest/UnitTestSolution.cs
+++ leetcodeCs/SolutionTest/UnitTestSolution.cs
@@ -4,13 +4,13 @@ using Solution;
 namespace SolutionTest
 {
 	[TestClass]
-	public class UnitTest1
+	public class UnitTestSolution
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void TestMakeSmallestPalindrome()
 		{
-			string[] input0 = {"egcfe", "abcd", "seven"};
-			string[] expect = {"efcfe", "abba", "neven"};
+			string[] input0 = { "egcfe", "abcd", "seven" };
+			string[] expect = { "efcfe", "abba", "neven" };
 			LeetcodeSolution sln = new();
 			for (int i = 0; i < input0.Length; i++)
 			{
