2aecdb539bd6af577202f0ce62d976f6572445c1
Shawn <yxiaoa@qq.com>
2024-05-29 09:40:00 +0800

    Add CountTestedDevices

--- leetcodeCs/Solution/LeetcodeSolution.cs
+++ leetcodeCs/Solution/LeetcodeSolution.cs
@@ -5,13 +5,29 @@ namespace Solution
 {
 	public class LeetcodeSolution
 	{
-		public string MakeSmallestPalindrome(string s) {
+		public int CountTestedDevices(int[] batteryPercentages)
+		{
+			int cnt = 0;
+			foreach (int b in batteryPercentages)
+			{
+				if (b > cnt)
+				{
+					cnt++;
+				}
+			}
+			return cnt;
+		}
+		public string MakeSmallestPalindrome(string s)
+		{
 			char[] chars = s.ToCharArray();
 			for (int i = 0, j = s.Length - 1; i < j; i++, j--)
 			{
-				if (chars[i] > chars[j]) {
+				if (chars[i] > chars[j])
+				{
 					chars[i] = chars[j];
-				} else {
+				}
+				else
+				{
 					chars[j] = chars[i];
 				}
 			}
--- leetcodeCs/SolutionTest/UnitTestSolution.cs
+++ leetcodeCs/SolutionTest/UnitTestSolution.cs
@@ -7,6 +7,17 @@ namespace SolutionTest
 	public class UnitTestSolution
 	{
 		[TestMethod]
+		public void TestCountTestedDevices()
+		{
+			int[][] input0 = { new int[] { 1, 1, 2, 1, 3 }, new int[] { 0, 1, 2 } };
+			int[] expect = { 3, 2 };
+			LeetcodeSolution sln = new();
+			for (int i = 0; i < input0.Length; i++)
+			{
+				Assert.AreEqual(expect[i], sln.CountTestedDevices(input0[i]));
+			}
+		}
+		[TestMethod]
 		public void TestMakeSmallestPalindrome()
 		{
 			string[] input0 = { "egcfe", "abcd", "seven" };
