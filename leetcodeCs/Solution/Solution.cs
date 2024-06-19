83e8f579c8d902bf1e7f371edc026f0aac38ed91
Shawn <yxiaoa@qq.com>
2024-06-19 09:56:01 +0800

    Add TestLeftRightDifference

--- leetcodeCs/Solution/Solution.cs
+++ leetcodeCs/Solution/Solution.cs
@@ -33,5 +33,18 @@ namespace Leetcode
 			}
 			return new string(chars);
 		}
+		public int[] LeftRightDifference(int[] nums)
+		{
+			int left = 0;
+			int right = nums.Sum();
+			int[] ret = new int[nums.Length];
+			for (int i = 0; i < nums.Length; i++)
+			{
+				right -= nums[i];
+				ret[i] = Math.Abs(right - left);
+				left += nums[i];
+			}
+			return ret;
+		}
 	}
 }
--- leetcodeCs/SolutionTest/UnitTestSolution.cs
+++ leetcodeCs/SolutionTest/UnitTestSolution.cs
@@ -28,5 +28,21 @@ namespace SolutionTest
 				Assert.AreEqual(expect[i], sln.MakeSmallestPalindrome(input0[i]));
 			}
 		}
+		[TestMethod]
+		public void TestLeftRightDifference()
+		{
+			int[][] input0 = { new int[] { 10, 4, 8, 3 }, new int[] { 1 } };
+			int[][] expect = { new int[] { 15, 1, 11, 22 }, new int[] { 0 } };
+			Solution sln = new();
+			for (int i = 0; i < input0.Length; i++)
+			{
+				int[] output = sln.LeftRightDifference(input0[i]);
+				Assert.AreEqual(expect[i].Length, output.Length);
+				for (int j = 0; j < output.Length; j++)
+				{
+					Assert.AreEqual(expect[i][j], output[j]);
+				}
+			}
+		}
 	}
 }
