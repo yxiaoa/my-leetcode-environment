684f378471fa78056d511a39314dee313fc848c4
Shawn <yxiaoa@qq.com>
2024-06-21 15:50:22 +0800

    Add minimumChairs for JAVA

--- leetcodeJava/src/Solution.java
+++ leetcodeJava/src/Solution.java
@@ -5,6 +5,20 @@ import java.text.*;
 import static java.lang.Integer.*;
 
 public class Solution {
+	public int minimumChairs(String s) {
+		int cnt = 0;
+		int max = 0;
+		for (char c : s.toCharArray()) {
+			if (c == 'E') {
+				cnt++;
+				max = Math.max(max, cnt);
+			} else {
+				cnt--;
+			}
+		}
+		return max;
+	}
+
 	public int strStr(String haystack, String needle) {
 		return haystack.indexOf(needle);
 	}
--- leetcodeJava/src/SolutionTest.java
+++ leetcodeJava/src/SolutionTest.java
@@ -4,6 +4,18 @@ import org.junit.Test;
 
 public class SolutionTest {
 	@Test
+	public void test_minimumChairs() {
+		String[] input0 = { "EEEEEEE", "ELELEEL", "ELEELEELLL" };
+		int[] expect = { 7, 2, 3 };
+
+		Solution sln = new Solution();
+
+		for (int i = 0; i < input0.length; i++) {
+			assertEquals(expect[i], sln.minimumChairs(input0[i]));
+		}
+	}
+
+	@Test
 	public void test_strStr() {
 		String[] input0 = { "sadbutsad", "leetcode" };
 		String[] input1 = { "sad", "leeto" };
