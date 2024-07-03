e7711edf06c9a5b5f7b64f5de46112469d28ed8a
Shawn <yxiaoa@qq.com>
2024-07-03 09:35:15 +0800

    Add tree lib for ruby

---
+++ leetcodeRuby/tree.rb
@@ -0,0 +1,12 @@
+class TreeNode
+        attr_accessor :val, :left, :right
+        def initialize(val = 0, left = nil, right = nil)
+                @val = val
+                @left = left
+                @right = right
+        end
+
+        def initialize(array)
+                
+        end
+end
