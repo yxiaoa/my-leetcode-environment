c47b4d9b85a7bf0fdbaac12f87ccb14d633bc976
Shawn <yxiaoa@qq.com>
2024-07-10 09:43:06 +0800

    Add gcdOfStrings

--- leetcodeDart/lib/solution.dart
+++ leetcodeDart/lib/solution.dart
@@ -21,6 +21,22 @@ class ListNode {
 }
 
 class Solution {
+  int _gcd(int a, int b) {
+    var c = a % b;
+    while (c != 0) {
+      a = b;
+      b = c;
+      c = a % b;
+    }
+    return b;
+  }
+
+  String gcdOfStrings(String str1, String str2) {
+    return ((str1 + str2) == (str2 + str1))
+        ? str1.substring(0, _gcd(str1.length, str2.length))
+        : "";
+  }
+
   int minimumOperations(List<int> nums) {
     return nums.where((n) => (0 != (n % 3))).length;
   }
--- leetcodeDart/test/solution_test.dart
+++ leetcodeDart/test/solution_test.dart
@@ -2,6 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:leetcode/solution.dart';
 
 void main() {
+  test("test gcdOfStrings", () {
+    var input0 = ["ABCABC", "ABABAB", "LEET"];
+    var input1 = ["ABC", "ABAB", "CODE"];
+    var expected = ["ABC", "AB", ""];
+
+    final sln = Solution();
+    for (var i = 0; i < input0.length; i++) {
+      var output = sln.gcdOfStrings(input0[i], input1[i]);
+      expect(output, expected[i]);
+    }
+  });
   test("test minimumOperations", () {
     var input0 = [
       [1, 2, 3, 4],
