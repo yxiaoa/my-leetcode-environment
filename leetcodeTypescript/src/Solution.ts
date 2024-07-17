5721b6435073eb3eb789610f87add9c82c4b6475
Shawn <yxiaoa@qq.com>
2024-07-17 09:49:39 +0800

    Add ListNode

--- leetcodeTypescript/src/Solution.ts
+++ leetcodeTypescript/src/Solution.ts
@@ -1,8 +1,44 @@
-export class Solution {
-	constructor() {
-
+export class ListNode {
+	val: number
+	next: ListNode | null
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = (val === undefined ? 0 : val)
+		this.next = (next === undefined ? null : next)
+	}
+	static array_to_list(num: number[]): ListNode {
+		var i = num.length - 1;
+		var head = new ListNode(num[i]);
+		while (i >= 0) {
+			var new_head = new ListNode(num[i], head);
+			head = new_head;
+			i--;
+		}
+		return head;
+	}
+	static list_to_array(head: ListNode | null): number[] {
+		var arr: number[] = [];
+		while (head != null) {
+			arr.push(head.val);
+			head = head.next;
+		}
+		return arr;
 	}
 }
+
+export class TreeNode {
+	val: number
+	left: TreeNode | null
+	right: TreeNode | null
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = (val === undefined ? 0 : val)
+		this.left = (left === undefined ? null : left)
+		this.right = (right === undefined ? null : right)
+	}
+}
+
+export function insertGreatestCommonDivisors(head: ListNode | null): ListNode | null {
+	return head;
+};
 
 export function countTestedDevices(batteryPercentages: number[]): number {
 	return batteryPercentages.reduce((cnt, b) => b > cnt ? cnt + 1 : cnt, 0);
--- leetcodeTypescript/test/Solution.test.ts
+++ leetcodeTypescript/test/Solution.test.ts
@@ -1,10 +1,31 @@
 import { expect, jest, test } from '@jest/globals';
-import { countTestedDevices } from '../src/Solution';
+import * as solution from '../src/Solution';
 
 test('countTestedDevices', () => {
 	var input0 = [[1, 1, 2, 1, 3], [0, 1, 2]];
 	var expected = [3, 2];
 	for (let i = 0; i < input0.length; i++) {
-		expect(countTestedDevices(input0[i])).toBe(expected[i]);
+		expect(solution.countTestedDevices(input0[i])).toBe(expected[i]);
 	}
 });
+
+test('insertGreatestCommonDivisors', () => {
+	var input0 = [[18, 6, 10, 3], [7]];
+	var expected = [[18, 6, 6, 2, 10, 1, 3], [7]];
+	for (let i = 0; i < input0.length; i++) {
+		var output = solution.insertGreatestCommonDivisors(solution.ListNode.array_to_list(input0[i]));
+		expect(solution.ListNode.list_to_array(output)).toBe(expected[i]);
+	}
+});
+
+//var time_ms = 0
+//if (optimize_time) {
+//	var start = performance.now()
+//	for (let cnt = 0; cnt < 100000; cnt++) {
+//		for (let i = 0; i < input0.length; i++) {
+//			var output = insertGreatestCommonDivisors(input0[i])
+//		}
+//	}
+//	var end = performance.now()
+//	time_ms += end - start
+//}
