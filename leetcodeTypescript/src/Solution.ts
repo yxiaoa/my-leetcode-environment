2213c75a8e1c183a64e11f634a25aedde76a73ac
Shawn <yxiaoa@qq.com>
2024-07-29 10:34:39 +0800

    Fix warnings

--- leetcodeTypescript/src/Solution.ts
+++ leetcodeTypescript/src/Solution.ts
@@ -2,21 +2,21 @@ export class ListNode {
 	val: number
 	next: ListNode | null
 	constructor(val?: number, next?: ListNode | null) {
-		this.val = (val === undefined ? 0 : val)
-		this.next = (next === undefined ? null : next)
+		this.val = (val ?? 0)
+		this.next = (next ?? null)
 	}
 	static array_to_list(num: number[]): ListNode {
-		var i = num.length - 1;
-		var head = new ListNode(num[i]);
+		let i = num.length - 1;
+		let head = new ListNode(num[i]);
 		while (i >= 0) {
-			var new_head = new ListNode(num[i], head);
+			let new_head = new ListNode(num[i], head);
 			head = new_head;
 			i--;
 		}
 		return head;
 	}
 	static list_to_array(head: ListNode | null): number[] {
-		var arr: number[] = [];
+		let arr: number[] = [];
 		while (head != null) {
 			arr.push(head.val);
 			head = head.next;
@@ -30,9 +30,9 @@ export class TreeNode {
 	left: TreeNode | null
 	right: TreeNode | null
 	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-		this.val = (val === undefined ? 0 : val)
-		this.left = (left === undefined ? null : left)
-		this.right = (right === undefined ? null : right)
+		this.val = (val ?? 0)
+		this.left = (left ?? null)
+		this.right = (right ?? null)
 	}
 }
 
--- leetcodeTypescript/test/Solution.test.ts
+++ leetcodeTypescript/test/Solution.test.ts
@@ -2,18 +2,18 @@ import { expect, jest, test } from '@jest/globals';
 import * as solution from '../src/Solution';
 
 test('countTestedDevices', () => {
-	var input0 = [[1, 1, 2, 1, 3], [0, 1, 2]];
-	var expected = [3, 2];
+	let input0 = [[1, 1, 2, 1, 3], [0, 1, 2]];
+	let expected = [3, 2];
 	for (let i = 0; i < input0.length; i++) {
 		expect(solution.countTestedDevices(input0[i])).toBe(expected[i]);
 	}
 });
 
 test('insertGreatestCommonDivisors', () => {
-	var input0 = [[18, 6, 10, 3], [7]];
-	var expected = [[18, 6, 6, 2, 10, 1, 3], [7]];
+	let input0 = [[18, 6, 10, 3], [7]];
+	let expected = [[18, 6, 6, 2, 10, 1, 3], [7]];
 	for (let i = 0; i < input0.length; i++) {
-		var output = solution.insertGreatestCommonDivisors(solution.ListNode.array_to_list(input0[i]));
+		let output = solution.insertGreatestCommonDivisors(solution.ListNode.array_to_list(input0[i]));
 		expect(solution.ListNode.list_to_array(output)).toBe(expected[i]);
 	}
 });
