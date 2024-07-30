6d641f77332efd2f20237946bbc239a0f4992d36
Shawn <yxiaoa@qq.com>
2024-07-30 10:05:10 +0800

    Add findIntersectionValues

--- leetcodeC/src/solution.c
+++ leetcodeC/src/solution.c
@@ -4,7 +4,7 @@
 
 char *makeSmallestPalindrome(char *s)
 {
-	int i = 0, j = strlen(s) - 1;
+	int i = 0, j = (int)strlen(s) - 1;
 	while (i < j) {
 		if (s[i] < s[j]) {
 			s[j] = s[i];
@@ -18,7 +18,29 @@ char *makeSmallestPalindrome(char *s)
 
 int *findIntersectionValues(int *nums1, int nums1Size, int *nums2, int nums2Size, int *returnSize)
 {
+	int i;
+	int map[2][101] = {0};
 	int *ret = (int *)malloc(2 * sizeof(int));
+
+	ret[0] = 0;
+	ret[1] = 0;
 	*returnSize = 2;
+
+	for (i = 0; i < nums1Size; i++) {
+		map[0][nums1[i]] = 1;
+	}
+	for (i = 0; i < nums2Size; i++) {
+		map[1][nums2[i]] = 1;
+	}
+	for (i = 0; i < nums1Size; i++) {
+		if (1 == map[1][nums1[i]]) {
+			ret[0]++;
+		}
+	}
+	for (i = 0; i < nums2Size; i++) {
+		if (1 == map[0][nums2[i]]) {
+			ret[1]++;
+		}
+	}
 	return ret;
 }
--- leetcodeC/test/test_solution.c
+++ leetcodeC/test/test_solution.c
@@ -5,13 +5,13 @@
 #include "solution.h"
 #include <string.h>
 
-#define getLen(arr)     (sizeof(arr) / sizeof(arr[0]))
-#define INPUT_SIZE      getLen(input)
-#define INPUT0_SIZE     getLen(input0)
-#define INPUT1_SIZE     getLen(input1)
-#define INPUT2_SIZE     getLen(input2)
-#define EXPECT_SIZE     getLen(expect)
-#define FOR_EACH(i)     for (int i = 0; i < INPUT_SIZE; i++)
+#define getLen(arr) (int)(sizeof(arr) / sizeof(arr[0]))
+#define INPUT_SIZE  getLen(input)
+#define INPUT0_SIZE getLen(input0)
+#define INPUT1_SIZE getLen(input1)
+#define INPUT2_SIZE getLen(input2)
+#define EXPECT_SIZE getLen(expect)
+#define FOR_EACH(i) for (int i = 0; i < INPUT_SIZE; i++)
 
 static char msg[512];
 
@@ -39,17 +39,27 @@ void test_makeSmallestPalindrome(void)
 void test_findIntersectionValues(void)
 {
 	int input01[] = {2, 3, 2}, input02[] = {1, 2}, expect0[] = {2, 1};
-	int *input[][2] = {{input01, input02}};
-	int inputSize[][2] = {{getLen(input01), getLen(input02)}};
-	int *expect[] = {expect0};
+	int input11[] = {4, 3, 2, 3, 1}, input12[] = {2, 2, 5, 2, 3, 6}, expect1[] = {3, 4};
+	int input21[] = {3, 4, 2, 3}, input22[] = {1, 5}, expect2[] = {0, 0};
+	int *input[][2] = {
+	        {input01, input02},
+                {input11, input12},
+                {input21, input22}
+        };
+	int inputSize[][2] = {
+	        {getLen(input01), getLen(input02)},
+	        {getLen(input11), getLen(input12)},
+	        {getLen(input21), getLen(input22)}
+        };
+	int *expect[] = {expect0, expect1, expect2};
 	int *output, retSize;
 
 	FOR_EACH (i) {
 		(void)sprintf(msg, "input %d\n", i);
-		output = findIntersectionValues(input[i][0], inputSize[i][0], input[i][1], inputSize[i][i], &retSize);
+		output = findIntersectionValues(input[i][0], inputSize[i][0], input[i][1], inputSize[i][1], &retSize);
 		TEST_ASSERT_EQUAL(2, retSize);
 		TEST_ASSERT_EQUAL_INT_ARRAY_MESSAGE(expect[i], output, retSize, msg);
 	}
 }
 
-#endif // TEST
+#endif   // TEST
