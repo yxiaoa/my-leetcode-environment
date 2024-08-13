5cc291c13e208e091d5b89fcdba5cbfe1fdf05c9
Shawn <yxiaoa@qq.com>
2024-08-13 10:07:45 +0800

    Add numPrimeArrangements

--- leetcodeDart/test/solution_test.dart
+++ leetcodeDart/test/solution_test.dart
@@ -2,6 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:leetcode/solution.dart';
 
 void main() {
+  test("test numPrimeArrangements", () {
+    var input0 = [5, 100];
+    var expected = [12, 682289015];
+
+    final sln = Solution();
+    for (var i = 0; i < input0.length; i++) {
+      var output = sln.numPrimeArrangements(input0[i]);
+      expect(output, expected[i]);
+    }
+  });
   test("test longestPalindrome", () {
     var input0 = ["abccccdd", "a"];
     var expected = [7, 1];
