d948cf4c26bb26d8142a5336c1d61509c4fa3354
Shawn <yxiaoa@qq.com>
2024-08-23 10:49:21 +0800

    Add findIntersectionValues

--- leetcodeDart/test/solution_test.dart
+++ leetcodeDart/test/solution_test.dart
@@ -2,6 +2,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:leetcode/solution.dart';
 
 void main() {
+  test("test findIntersectionValues", () {
+    var input0 = [
+      [2, 3, 2],
+      [4, 3, 2, 3, 1],
+      [3, 4, 2, 3]
+    ];
+    var input1 = [
+      [1, 2],
+      [2, 2, 5, 2, 3, 6],
+      [1, 5]
+    ];
+    var expected = [
+      [2, 1],
+      [3, 4],
+      [0, 0]
+    ];
+    final sln = Solution();
+    for (var i = 0; i < input0.length; i++) {
+      var output = sln.findIntersectionValues(input0[i], input1[i]);
+      expect(output, expected[i]);
+    }
+  });
   test("test numPrimeArrangements", () {
     var input0 = [5, 100];
     var expected = [12, 682289015];
