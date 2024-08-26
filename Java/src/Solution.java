731701e40b9a74019428eb931f422f05beb865d1
Shawn <yxiaoa@qq.com>
2024-08-26 10:01:54 +0800

    Add findIntersectionValues

--- Java/src/Solution.java
+++ Java/src/Solution.java
@@ -5,6 +5,29 @@ import java.text.*;
 import static java.lang.Integer.*;
 
 public class Solution {
+	public int[] findIntersectionValues(int[] nums1, int[] nums2) {
+		int cnt1 = 0;
+		int cnt2 = 0;
+		Set<Integer> set1 = new HashSet<>();
+		Set<Integer> set2 = new HashSet<>();
+		for (int n : nums1) {
+			set1.add(n);
+		}
+		for (int n : nums2) {
+			set2.add(n);
+		}
+		for (int n : nums1) {
+			if (set2.contains(n)) {
+				cnt1++;
+			}
+		}
+		for (int n : nums2) {
+			if (set1.contains(n)) {
+				cnt2++;
+			}
+		}
+		return new int[] { cnt1, cnt2 };
+	}
 	public int longestPalindrome(String s) {
 		int[] map = new int[58];
 		int sum = 0;
