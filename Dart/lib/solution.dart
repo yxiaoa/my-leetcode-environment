e6581458c705a339c028f2fbb4ef925d5005c926
Shawn <yxiaoa@qq.com>
2024-09-24 09:54:04 +0800

    Add surfaceArea for dart

--- Dart/lib/solution.dart
+++ Dart/lib/solution.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class ListNode {
   int val;
   ListNode? next;
@@ -21,6 +23,28 @@ class ListNode {
 }
 
 class Solution {
+  int surfaceArea(List<List<int>> grid) {
+    var n = grid.length;
+    var area = 0;
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        if (grid[i][j] > 0) {
+          area += 2;
+          for (var k = 0; k < 4; k++) {
+            var x = i + [0, 0, 1, -1][k];
+            var y = j + [1, -1, 0, 0][k];
+            var val = 0;
+            if (x >= 0 && x < n && y >= 0 && y < n) {
+              val = grid[x][y];
+            }
+            area += max(0, grid[i][j] - val);
+          }
+        }
+      }
+    }
+    return area;
+  }
+
   String removeTrailingZeros(String num) {
     return num.replaceAll(RegExp(r'0+$'), '');
   }
--- Dart/test/solution_test.dart
+++ Dart/test/solution_test.dart
@@ -2,6 +2,34 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:leetcode/solution.dart';
 
 void main() {
+  test("test surfaceArea", () {
+    var input0 = [
+      [
+        [1, 2],
+        [3, 4]
+      ],
+      [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1]
+      ],
+      [
+        [2]
+      ],
+      [
+        [2, 2, 2],
+        [2, 1, 2],
+        [2, 2, 2]
+      ]
+    ];
+    var expected = [34, 32, 10, 46];
+
+    final sln = Solution();
+    for (var i = 0; i < input0.length; i++) {
+      var output = sln.surfaceArea(input0[i]);
+      expect(output, expected[i]);
+    }
+  });
   test("test removeTrailingZeros", () {
     var input0 = ["51230100", "123", "000"];
     var expected = ["512301", "123", ""];
