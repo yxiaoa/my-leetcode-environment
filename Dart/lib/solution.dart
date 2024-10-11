e532d92bb62006da6265f58e49239e18b37ebf91
Shawn <yxiaoa@qq.com>
2024-10-11 15:34:25 +0800

    Add shortestToChar for Dart

--- Dart/lib/solution.dart
+++ Dart/lib/solution.dart
@@ -23,6 +23,26 @@ class ListNode {
 }
 
 class Solution {
+  List<int> shortestToChar(String s, String c) {
+    var n = s.length;
+    var res = List.filled(n, 0);
+    var prev = -n;
+    for (var i = 0; i < n; i++) {
+      if (s[i] == c) {
+        prev = i;
+      }
+      res[i] = i - prev;
+    }
+    prev = 2 * n;
+    for (var i = n - 1; i >= 0; i--) {
+      if (s[i] == c) {
+        prev = i;
+      }
+      res[i] = min(res[i], prev - i);
+    }
+    return res;
+  }
+
   int surfaceArea(List<List<int>> grid) {
     var n = grid.length;
     var area = 0;
--- Dart/test/solution_test.dart
+++ Dart/test/solution_test.dart
@@ -2,6 +2,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:leetcode/solution.dart';
 
 void main() {
+  test("test shortestToChar", () {
+    var input0 = ["loveleetcode", "aaab", "a"];
+    var input1 = ["e", "b", "a"];
+    var expected = [
+      [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0],
+      [3, 2, 1, 0],
+      [0]
+    ];
+
+    final sln = Solution();
+    for (var i = 0; i < input0.length; i++) {
+      var output = sln.shortestToChar(input0[i], input1[i]);
+      expect(output, expected[i]);
+    }
+  });
   test("test surfaceArea", () {
     var input0 = [
       [
