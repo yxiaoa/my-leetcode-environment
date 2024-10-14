eb6385a08ada7003c2b71f17f8745d716fd7b45d
Shawn <yxiaoa@qq.com>
2024-10-14 10:04:48 +0800

    Add shortestToChar for java

--- Java/src/Solution.java
+++ Java/src/Solution.java
@@ -5,6 +5,25 @@ import java.text.*;
 import static java.lang.Integer.*;
 
 public class Solution {
+	public int[] shortestToChar(String s, char c) {
+		int n = s.length();
+		int[] res = new int[n];
+		int pos = -n;
+		for (int i = 0; i < n; i++) {
+			if (s.charAt(i) == c) {
+				pos = i;
+			}
+			res[i] = i - pos;
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			if (s.charAt(i) == c) {
+				pos = i;
+			}
+			res[i] = Math.min(res[i], Math.abs(i - pos));
+		}
+		return res;
+	}
+
 	public String removeTrailingZeros(String num) {
 		return num.replaceAll("0*$", "");
 	}
--- Java/src/SolutionTest.java
+++ Java/src/SolutionTest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 
 public class SolutionTest {
 	@Test
+	public void test_shortestToChar() {
+		String[] input0 = { "loveleetcode", "aaab", "aaba" };
+		char[] input1 = { 'e', 'b', 'b' };
+		int[][] expect = { { 3,2,1,0,1,0,0,1,2,2,1,0 }, { 3, 2, 1, 0 }, { 2, 1, 0, 1 } };
+		Solution sln = new Solution();
+		for (int i = 0; i < input0.length; i++) {
+			assertArrayEquals(expect[i], sln.shortestToChar(input0[i], input1[i]));
+		}
+	}
+
+	@Test
 	public void test_removeTrailingZeros() {
 		String[] input0 = { "51230100", "123" };
 		String[] expect = { "512301", "123" };
