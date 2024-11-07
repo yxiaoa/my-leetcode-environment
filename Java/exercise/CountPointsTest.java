81188c20bc5df7b5b7c9a4eba910e8faf3aebc69
Shawn <yxiaoa@qq.com>
2024-11-07 09:56:15 +0800

    Add CountPoints for Java

---
+++ Java/exercise/CountPointsTest.java
@@ -0,0 +1,23 @@
+package exercise;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class CountPointsTest {
+	@Test
+	public void testCountPointsExample1() {
+		int[][] input0 = { { 1, 3 }, { 3, 3 }, { 5, 3 }, { 2, 2 } };
+		int[][] input1 = { { 2, 3, 1 }, { 4, 3, 1 }, { 1, 1, 2 } };
+		int[] expect = { 3, 2, 2 };
+		Solution sln = new Solution();
+		assertArrayEquals(expect, sln.countPoints(input0, input1));
+	}
+	@Test
+	public void testCountPointsExample2() {
+		int[][] input0 = { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 }, { 5, 5 } };
+		int[][] input1 = { { 1, 2, 2 }, { 2, 2, 2 }, { 4, 3, 2 }, { 4, 3, 3 } };
+		int[] expect = { 2, 3, 2, 4 };
+		Solution sln = new Solution();
+		assertArrayEquals(expect, sln.countPoints(input0, input1));
+	}
+}
