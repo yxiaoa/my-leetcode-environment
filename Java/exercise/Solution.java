43769091c1eefd6b3afbae9fb08ae671fc369d99
Shawn <yxiaoa@qq.com>
2025-02-03 08:17:48 +0800

    Add stableMountains method and corresponding unit test

--- Java/exercise/Solution.java
+++ Java/exercise/Solution.java
@@ -3,6 +3,16 @@ package exercise;
 import java.util.*;
 
 public class Solution {
+	public List<Integer> stableMountains(int[] height, int threshold) {
+		List<Integer> res = new ArrayList<>();
+		for (int i = 1; i < height.length; i++) {
+			if (height[i - 1] > threshold) {
+				res.add(i);
+			}
+		}
+		return res;
+	}
+
 	public int deleteGreatestValue(int[][] grid) {
 		for (int[] row : grid) {
 			Arrays.sort(row);
--- Java/test/SolutionTest.java
+++ Java/test/SolutionTest.java
@@ -4,12 +4,24 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.Test;
 
 import exercise.Solution;
 
 public class SolutionTest {
+	@Test
+	public void teststableMountains() {
+		int[][] input0 = { { 1, 2, 3, 4, 5 }, { 10, 1, 10, 1, 10 }, { 10, 1, 10, 1, 10 } };
+		int[] input1 = { 2, 3, 10 };
+		int[][] expect = { { 3, 4 }, { 1, 3 }, {} };
+		Solution sln = new Solution();
+		for (int i = 0; i < input0.length; i++) {
+			assertArrayEquals(expect[i],
+					sln.stableMountains(input0[i], input1[i]).stream().mapToInt(Integer::intValue)
+							.toArray());
+		}
+	}
+
 	@Test
 	public void testDeleteGreatestValue() {
 		int[][][] input0 = { { { 1, 2, 4 }, { 3, 3, 1 } }, { { 10 } } };
