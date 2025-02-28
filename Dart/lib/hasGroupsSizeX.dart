cc65a606fa1ddab6035df39f8a8344dfd587cd22
Shawn <yxiaoa@qq.com>
2025-02-28 14:58:07 +0800

    Add hasGroupsSizeX method to determine if deck can be partitioned into groups of equal size

---
+++ Dart/lib/hasGroupsSizeX.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+class Solution {
+  bool hasGroupsSizeX(List<int> deck) {
+    var count = <int, int>{};
+    for (var card in deck) {
+      count[card] = (count[card] ?? 0) + 1;
+    }
+    var gcd = count.values.first;
+    for (var value in count.values) {
+      gcd = _gcd(gcd, value);
+      if (gcd == 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  int _gcd(int a, int b) {
+    while (b != 0) {
+      var t = b;
+      b = a % b;
+      a = t;
+    }
+    return a;
+  }
+}
