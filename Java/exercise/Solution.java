ab473f290fbc62c2a4b5b9e234083605ae78a353
Shawn <yxiaoa@qq.com>
2025-05-02 10:57:15 +0800

    Add findErrorNums function and corresponding unit test in Java

--- Java/exercise/Solution.java
+++ Java/exercise/Solution.java
@@ -3,6 +3,25 @@ package exercise;
 import java.util.*;
 
 public class Solution {
+	public int[] findErrorNums(int[] nums) {
+		int[] res = new int[2];
+		int n = nums.length;
+		int sum = 0;
+		int sumSet = 0;
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			if (!set.add(nums[i])) {
+				res[0] = nums[i];
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			sumSet += i;
+		}
+		res[1] = res[0] + sumSet - sum;
+		return res;
+	}
+
 	public int finalPositionOfSnake(int n, List<String> commands) {
 		int pos = 0;
 		for (String command : commands) {
--- Java/test/SolutionTest.java
+++ Java/test/SolutionTest.java
@@ -10,6 +10,16 @@ import exercise.Solution;
 
 public class SolutionTest {
 	@Test
+	public void test_findErrorNums() {
+		int[][] input0 = { { 1, 2, 2, 4 }, { 3, 2, 3, 4, 6, 5 }, { 1, 1 }, { 2, 2 } };
+		int[][] expect = { { 2, 3 }, { 3, 1 }, { 1, 2 }, { 2, 1 } };
+		Solution sln = new Solution();
+		for (int i = 0; i < input0.length; i++) {
+			assertArrayEquals(expect[i], sln.findErrorNums(input0[i]));
+		}
+	}
+
+	@Test
 	public void testfinalPositionOfSnake() {
 		int[] input0 = { 2, 3 };
 		String[][] input1 = { { "RIGHT", "DOWN" }, { "DOWN", "RIGHT", "UP" } };
