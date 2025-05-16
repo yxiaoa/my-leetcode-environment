58e56f10b6b2ce5542b8549b79f5debba5a38f56
Shawn <yxiaoa@qq.com>
2025-05-16 09:50:34 +0800

    Add hasGroupsSizeX method and corresponding unit test in Java

--- Java/exercise/Solution.java
+++ Java/exercise/Solution.java
@@ -3,6 +3,30 @@ package exercise;
 import java.util.*;
 
 public class Solution {
+	private int gcd(int a, int b) {
+		while (b != 0) {
+			int t = b;
+			b = a % b;
+			a = t;
+		}
+		return a;
+	}
+
+	public boolean hasGroupsSizeX(int[] deck) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int n : deck) {
+			map.put(n, map.getOrDefault(n, 0) + 1);
+		}
+		int gcd = 0;
+		for (int count : map.values()) {
+			gcd = gcd(gcd, count);
+			if (gcd == 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public int[] findErrorNums(int[] nums) {
 		int[] res = new int[2];
 		int n = nums.length;
--- Java/test/SolutionTest.java
+++ Java/test/SolutionTest.java
@@ -10,6 +10,16 @@ import exercise.Solution;
 
 public class SolutionTest {
 	@Test
+	public void test_hasGroupsSizeX() {
+		int[][] input0 = { { 1, 2, 3, 4, 4, 3, 2, 1 }, { 1, 1, 1, 2, 2, 2, 3, 3 } };
+		boolean[] expect = { true, false };
+		Solution sln = new Solution();
+		for (int i = 0; i < input0.length; i++) {
+			assertEquals(expect[i], sln.hasGroupsSizeX(input0[i]));
+		}
+	}
+
+	@Test
 	public void test_findErrorNums() {
 		int[][] input0 = { { 1, 2, 2, 4 }, { 3, 2, 3, 4, 6, 5 }, { 1, 1 }, { 2, 2 } };
 		int[][] expect = { { 2, 3 }, { 3, 1 }, { 1, 2 }, { 2, 1 } };
