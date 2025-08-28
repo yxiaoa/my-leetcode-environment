39f664c46bf9c85cf6f532f6a138b941c2777d10
Shawn <yxiaoa@qq.com>
2025-08-28 09:07:20 +0800

    Refactor canAliceWin method to simplify sum calculation and remove unnecessary variable

--- Java/exercise/canAliceWin/Solution.java
+++ Java/exercise/canAliceWin/Solution.java
@@ -3,13 +3,9 @@ package exercise.canAliceWin;
 public class Solution {
 	public boolean canAliceWin(int[] nums) {
 		int sum = 0;
-		int sumSingle = 0;
 		for (int i = 0; i < nums.length; i++) {
-			sum += nums[i];
-			if (nums[i] < 10) {
-				sumSingle += nums[i];
-			}
+			sum += (nums[i] < 10) ? nums[i] : -nums[i];
 		}
-		return sum != (sumSingle * 2);
+		return sum != 0;
 	}
 }
