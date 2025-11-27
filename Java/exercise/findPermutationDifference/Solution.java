f480c641f95a276deb4856c161de296a031e773c
Shawn <yxiaoa@qq.com>
2025-11-27 10:06:45 +0800

    Add Solution class with findPermutationDifference method and corresponding unit tests

---
+++ Java/exercise/findPermutationDifference/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+	public int findPermutationDifference(String s, String t) {
+		int sum = 0;
+		int map[] = new int[26];
+		for (int i = 0; i < s.length(); i++) {
+			map[s.charAt(i) - 'a'] = i;
+		}
+		for (int i = 0; i < t.length(); i++) {
+			sum += Math.abs(map[t.charAt(i) - 'a'] - i);
+		}
+		return sum;
+	}
+}
---
+++ Java/exercise/findPermutationDifference/SolutionTest.java
@@ -0,0 +1,23 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+	@Test
+	public void test_example_1() {
+		String input0 = "abc";
+		String input1 = "bac";
+		int expect = 2;
+		Solution sln = new Solution();
+		assertEquals(expect, sln.findPermutationDifference(input0, input1));
+	}
+
+	@Test
+	public void test_example_2() {
+		String input0 = "abcde";
+		String input1 = "edbac";
+		int expect = 12;
+		Solution sln = new Solution();
+		assertEquals(expect, sln.findPermutationDifference(input0, input1));
+	}
+}
